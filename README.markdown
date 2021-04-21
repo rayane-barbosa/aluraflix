64d67f3a81744cea83ae7efb02f84e9b69d0108a
rayane-barbosa <rayane.c.barbosa@gmail.com>
2021-04-21 19:24:48 -0300

    aluraflix imersão dev

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# AluraFlix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rayane-barbosa/pen/dyNpXPQ](https://codepen.io/rayane-barbosa/pen/dyNpXPQ).
+
+#### Projeto realizado para a imersão dev da alura realizada em abr/2021 focada em programação JavaScript realizada no codepen
+
---
+++ src/script.js
@@ -0,0 +1,27 @@
+//var filme1 = "Star wars" declaração inicial de variáveis
+//var filme2 ="Toy Story"  ||     ||
+//var filme3 ="Interestellar"      ||   ||
+// console.log (filme1) impressão inicial de variaveis
+//console.log (filme2) ||    ||
+//console.log (filme3)  ||    ||
+//var filmes ["Star wars","Toy Story","Interestellar"] array = vetor de variáveis
+//console.log(filmes) imprime todos os valores
+//arraway começa em posição 0
+//console.log (filmes[0]) imprime somente o valor da posição idicada
+//for (i --) decrescente  || (i++) crescente
+//for (var i = 0; i<= n; i++) ordem decrescente || for (var i= 0; i <= n; i++) ordem crescente || for (var i = 0; i < filmes.length; i++) busca o tamanho da array
+//filmes.lenght busca o tamanho da variavel no array
+//filmes.push ("adicionar novo elemento no array")
+
+var filmes = ["Watchmen", "De Volta para o Futuro", "Pulp Fiction", "Jurassic Park", "Bastardos Inglórios", "Clube da Luta", "O Exterminador do Futuro 2", "As Vantagens de Ser Invisível", "Donnie Darko", "Trainspotting"] //variavel com os nomes dos filmes
+
+var filmesimage = ["https://m.media-amazon.com/images/M/MV5BY2IzNGNiODgtOWYzOS00OTI0LTgxZTUtOTA5OTQ5YmI3NGUzXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BZThjMmQ5YjktMTUyMC00MjljLWJmMTAtOWIzNDIzY2VhNzQ0XkEyXkFqcGdeQXVyMTAyNjg4NjE0._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BZjZlZDlkYTktMmU1My00ZDBiLWFlNjEtYTBhNjVhOTM4ZjJjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"] // variavel contendo capa do filme
+
+filmes.push("Star Wars")
+filmesimage.push("https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg")// para inserir os elementos é necessário que seja antes do for 
+filmes.push("Seven")
+filmesimage.push("https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg")
+for (var i = 0; i < filmesimage.length; i++){
+  document.write("<img src=" + filmesimage[i] + ">")
+}
+
